ce937994310da1f8b88a70029ba8a67413087750
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-06-04 21:35:37 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+
 
 export const TOKEN_KEY = "smartbank_token";
 export const USER_KEY = "smartbank_user";
